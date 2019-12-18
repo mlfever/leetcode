4a3e5f1c42e39237afc6a90a678bbdb52cfcf978
mlfever <reter216@126.com>
2019-12-18 10:42:29 +0800

    split file folder

---
+++ c/link_list.c
@@ -0,0 +1,12 @@
+/**
+ * Definition for singly-linked list.
+ */
+ struct ListNode {
+     int val;
+     struct ListNode *next;
+ };
+
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+
+}
