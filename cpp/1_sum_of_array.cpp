6ec6d7c6d3de35bca63f1cc1a009bb6001fd0119
jowang <wang@JOW-LAPTOP.localdomain>
2022-01-10 22:02:56 +0800

    use hash to lookup

---
+++ cpp/1_sum_of_array.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+	    unordered_map<int, int> hashtable;
+	    for (int i = 0; i < nums.size(); ++i) {
+	    	auto it = hashtable.find(target - nums[i]);
+		if (it != hashtable.end()) {
+			return {it->second, i};
+		}
+		hashtable[nums[i]] = i;
+	    }
+	    return {};
+    }
+};
+
+int main()
+{
+	Solution a;
+    vector<int> array = {2, 7, 11, 15};
+    vector<int> result;
+
+    result = a.twoSum(array, 17);
+    if (!result.empty()) {
+    	for (auto it : result) {
+		cout << it;
+	}
+    }
+
+
+
+    return 0;
+}
